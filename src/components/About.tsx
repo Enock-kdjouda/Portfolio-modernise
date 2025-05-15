2376c22ba11920461084911548f14ead52bf3a35
enock <enockdjoudas@gmail.com>
2025-05-15 23:33:07 +0200

    Dossier images , public créer commit (assets)

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -47,7 +47,7 @@ const About = () => {
             </div>
 
             <a
-              href="../dist/assets/images/ENOCK VIVIEN A. KPADJOUDA .pdf"
+              href="/assets/images/ENOCK VIVIEN A. KPADJOUDA .pdf"
               className="inline-block mt-8 px-6 py-3 bg-blue-600 hover:bg-blue-700 text-white rounded-full transition-colors duration-300 font-medium"
             >
               Voir mon CV
